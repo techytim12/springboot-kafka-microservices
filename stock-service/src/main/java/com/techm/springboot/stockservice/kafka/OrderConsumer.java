30fd2737fab1d6b4c5ada6b14027577118370ba1
Thomas Philip <thomasphilipchry@gmail.com>
2024-03-19 10:04:15 +0530

    Configured stock-service for a consumer

---
+++ stock-service/src/main/java/com/techm/springboot/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,19 @@
+package com.techm.springboot.stockservice.kafka;
+
+import com.techm.springboot.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event){
+        LOGGER.info(String.format("order event received from stock service -> %s", event.toString()));
+    }
+}
